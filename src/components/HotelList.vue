1c7c23b000cb363fc76a360173c047828f9b8df3
Wanny Miarelli <wannymiarelli@live.com>
2017-01-20 11:28:37 +0100

    first push

---
+++ src/components/HotelList.vue
@@ -0,0 +1,97 @@
+<template lang="html">
+  <div>
+    <q-data-table
+      :data="table"
+      :config="config"
+      :columns="columns"
+    >
+      <!-- Custom renderer for "message" column -->
+      <template slot="col-message" scope="cell">
+        <span class="light-paragraph">{{cell.data}}</span>
+      </template>
+      <!-- Custom renderer for "source" column -->
+      <template slot="col-source" scope="cell">
+        <span v-if="cell.data === 'Audit'" class="label text-white bg-primary">
+          Audit
+          <q-tooltip>Some data</q-tooltip>
+        </span>
+        <span v-else class="label text-white bg-negative">{{cell.data}}</span>
+      </template>
+      <!-- Custom renderer when user selected one or more rows -->
+      <template slot="selection" scope="selection">
+        <button class="primary clear" @click="changeMessage(selection)">
+          <i>edit</i>
+        </button>
+        <button class="primary clear" @click="deleteRow(selection)">
+          <i>delete</i>
+        </button>
+      </template>
+    </q-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.$http.get('https://api.ermeshotels.com/api/v1/hotel/active').then((response) => {
+      this.table = response.body.data
+    }, (error) => {
+      console.log(error)
+    })
+  },
+  data () {
+    return {
+      config: {
+        rowHeight: '50px',
+        title: 'Hotels',
+        refresh: true,
+        columnPicker: true,
+        responsive: true,
+        pagination: {
+          rowsPerPage: 15,
+          options: [50, 100, 150]
+        }
+      },
+      columns: [
+        {
+          label: 'Nome',
+          field: 'name',
+          filter: true
+        },
+        {
+          label: 'Tipo',
+          field: 'type',
+          filter: true
+        },
+        {
+          label: 'Indirizzo',
+          field: 'address',
+          filter: true
+        },
+        {
+          label: 'Cap',
+          field: 'zip',
+          filter: true
+        },
+        {
+          label: 'Località',
+          field: 'location',
+          filter: true
+        },
+        {
+          label: 'Utente',
+          field: 'users',
+          filter: true,
+          format: (value) => {
+            return value.join(',')
+          }
+        }
+      ],
+      table: []
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-layout>
+
+    <div slot="header" class="toolbar">
+      <button class="hide-on-drawer-visible" @click="$refs.leftDrawer.open()">
+        <i>menu</i>
+      </button>
+      <q-toolbar-title :padding="1">
+        ErmesScout
+      </q-toolbar-title>
+    </div>
+
+    <q-tabs slot="navigation">
+      <q-tab icon="domain" route="/hotel/list" exact replace>Hotels</q-tab>
+    </q-tabs>
+
+    <q-drawer left-side swipe-only ref="leftDrawer">
+      <div class="toolbar light">
+        <q-toolbar-title :padding="1">
+            Right-side Drawer
+        </q-toolbar-title>
+      </div>
+
+      <p style="padding: 25px;" class="text-grey-7">
+        This is yet another Drawer that does not gets displayed alongside content on
+        bigger screens.
+      </p>
+    </q-drawer>
+
+    <router-view class="layout-view"></router-view>
+
+  </q-layout>
+</template>
+
+<script>
+</script>
+
+<style lang="styl">
+.logo-container
+  width 192px
+  height 268px
+  perspective 800px
+  position absolute
+  top 50%
+  left 50%
+  transform translateX(-50%) translateY(-50%)
+.logo
+  position absolute
+  transform-style preserve-3d
+</style>
